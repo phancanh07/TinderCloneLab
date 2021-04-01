ce7a79c610f055d47732323f39609af2ed7c3cb5
toitenduccanh <68606005+toitenduccanh@users.noreply.github.com>
2021-04-01 22:54:38 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express =require('express');
+var expressHbs=require('express-handlebars');
+var app= express();
+app.engine('handlebars',expressHbs({
+    layoutsDir:__dirname+'/views/layouts',
+    defaultLayout: 'appweb'
+}));
+
+app.set('view engine','handlebars');
+app.use(express.static(  'assets'));
+
+app.get('/',function(req,res){
+res.render('index',{layout:'indexmain'});
+});
+app.get('/fiend.handlebars',function (req,res) {
+    res.render("friend");
+
+});
+app.get('/friend.handlebars',function (req,res) {
+    res.render("friend");
+});
+
+app.get('/login.handlebars',function (req,res) {
+    res.render("login",{layout: 'main',});
+
+});
+app.get('/profile.handlebars',function (req,res)  {
+    res.render('profile');
+})
+
+app.listen(process.env.PORT || '2222');
